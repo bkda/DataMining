c4847308cfccefa93876e6f15acef95107140f7d
Tracy <tracyliubai@gmail.com>
2016-05-22 13:53:26 +0800

    Initial commit

--- Algorithm/Viterbi.py
+++ Algorithm/Viterbi.py
@@ -4,6 +4,7 @@
     Mail tracyliubai@gmail.com
 '''
 
+
 def viterbi(observ, states, start_prob, trans_prob, emit_prob):
     '''
     :param obs:         observations set tuple
@@ -26,7 +27,16 @@ def viterbi(observ, states, start_prob, trans_prob, emit_prob):
             V[i][j] = prob
             newpath[j] = path[state] + [j]
         path = newpath
+    printNicely(V)
     (prob, state) = max([(V[len(observ) - 1][y], y) for y in states])
     return (prob, path[state])
 
 
+def printNicely(v):
+    for i in range(0, len(v) + 1): print('{:^7}'.format(i), end='')
+    for i in v[0].keys():
+        print()
+        d = [round(v[j][i], 7) for j in range(len(v))]
+        d.insert(0, i)
+        for j in d: print('{:^7}'.format(j), end='')
+    print('\n')
